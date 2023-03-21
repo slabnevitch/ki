19df8c145692e7bcd11a0a64a66262f9aeead94d
slabnevitch <handofschpilrain@mail.ru>
2023-03-21 18:05:42 +0500

    menu__026 (contacts) ready

--- js/yamap-fullpage.js
+++ js/yamap-fullpage.js
@@ -4,6 +4,9 @@ if (document.getElementById('map') !== null){
 if (document.getElementById('map-shops') !== null){
   ymaps.ready(shopsMapInit);
 }
+if (document.getElementById('map-single-shop') !== null){
+  ymaps.ready(singleShopsMapInit);
+}
 function fullMapInit(){     
 	var myMap = new ymaps.Map("map", {
 		center: [45.047920, 38.974579],
@@ -21,6 +24,20 @@ function fullMapInit(){
 
 function shopsMapInit(){     
   var myMap = new ymaps.Map("map-shops", {
+    center: [45.047920, 38.974579],
+    zoom: 15,
+        controls: ['zoomControl'], //оставляем только масштабирование
+            // behaviors: ['drag'] //оставляем только поведение drag
+      // autoFitToViewport: 'always'
+  }, {
+        // При сложных перестроениях можно выставить автоматическое
+        // обновление карты при изменении размеров контейнера.
+        // При простых изменениях размера контейнера рекомендуется обновлять карту программно.
+        autoFitToViewport: 'always'
+    });
+}
+function singleShopsMapInit(){     
+  var myMap = new ymaps.Map("map-single-shop", {
     center: [45.047920, 38.974579],
     zoom: 15,
         controls: ['zoomControl'], //оставляем только масштабирование
